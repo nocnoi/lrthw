90e3a71de4ff8c98994df0fe67d07f82a9dfa899
Nisha Salim <nocturne.noire@gmail.com>
2020-02-09 11:22:09 +0530

    ex1 puts exercise

---
+++ ex1.rb
@@ -0,0 +1,10 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing like this"
+puts "This is fun."
+puts 'Yay! Printing.'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this'
+
+# starting a line with a # (octothorpe) character treats the line as a comment
+# octothorpe is also called pound, hash, or mesh. Pick whatever rocks your boat!
